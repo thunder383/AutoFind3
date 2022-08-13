92bda0391f48a65b121ef10e1ee6a80d0afbefea
Omar Butt <1612982@brunel.ac.uk>
2022-08-13 11:03:46 +0100

    Initial commit

---
+++ src/main/java/com/patel/autofind3/AutoFind3Application.java
@@ -0,0 +1,13 @@
+package com.patel.autofind3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFind3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoFind3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/patel/autofind3/AutoFind3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patel.autofind3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFind3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
